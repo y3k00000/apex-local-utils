684c0fe9cf3357de783516f99e585803a7e670ab
y3k <y3k00000@gmail.com>
2024-12-24 17:26:41 +0800

    241224 update tests

--- test/main.go
+++ test/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
@@ -28,13 +30,36 @@ func main() {
 	// 	fmt.Println(expire)
 	// }
 
+	// PHP time test
+	parsePHPTime := func(phpTime string) (time.Time, error) {
+		return time.Parse("2006-01-02 15:04:05", phpTime)
+	}
+	for y := 1975; y < 2050; y++ {
+		for m := 1; m < 13; m++ {
+			for d := 1; d < 29; d++ {
+				dateString := fmt.Sprintf("%d-%02d-%02d %02d:%02d:%02d", y, m, d, rand.Intn(12), rand.Intn(60), rand.Intn(60))
+				date, err := parsePHPTime(dateString)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println(date.Format(time.RubyDate))
+				}
+			}
+		}
+	}
+
 	// json marshal error test
-	type TestStruct struct {
-		Name string `json:"name"`
-	}
-	TestStructInstance := TestStruct{Name: ""}
-	err := json.Unmarshal([]byte(`{}`), &TestStructInstance)
-	if err != nil {
-		panic(err)
-	}
+	// type TestStruct struct {
+	// 	Name *string `json:"name"`
+	// }
+	// TestStructInstance := TestStruct{}
+	// if err := json.Unmarshal([]byte(`{"name":null}`), &TestStructInstance); err != nil {
+	// 	panic(err)
+	// }
+
+	// if json, err := json.Marshal(TestStructInstance); err != nil {
+	// 	panic(err)
+	// } else {
+	// 	println(string(json))
+	// }
 }
